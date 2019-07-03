09b3da6189cb555260368e7996f3bb3f61ed63d7
K T Prajwal Prathiksh <47190367+Jamun-Fanatic-Foreva@users.noreply.github.com>
2019-07-03 17:17:18 +0530

    Updated tc_step_sampling
    tc_step_sampling utilized np.linspace on arrays as input before, which is not supported in previous python version.
    Had to implement changes for it to work on all versions.

---
+++ tc_step_sampling_updated.py
@@ -0,0 +1,26 @@
+import numpy as np
+from constants_1U import PWM_FREQUENCY, RESISTANCE, INDUCTANCE
+
+def getTimeArr(duty_cycle):
+    if np.shape(duty_cycle) == ():
+        peak_time = np.abs(duty_cycle)/PWM_FREQUENCY
+        time_constant = INDUCTANCE / RESISTANCE
+        time_prev = np.linspace(peak_time / 4, peak_time, 3, endpoint=False)
+        time_tc = np.linspace(peak_time + time_constant/3, peak_time + time_constant, 3, endpoint=True)
+        time_post = np.linspace((peak_time + time_constant)*2/3 + 1/PWM_FREQUENCY/3, 1/PWM_FREQUENCY*0.9999, 3, endpoint=True)
+        time_arr = np.concatenate((time_prev, time_tc, time_post))
+        time_arr1 = time_arr.flatten()
+        time_arr1 = np.concatenate((time_arr1, np.unique(peak_time)))
+        time_arr1 = np.append(time_arr1, 1/PWM_FREQUENCY)
+        return np.unique(time_arr1)
+    else:
+        return getTimeArr_Vector(duty_cycle)
+
+def getTimeArr_Vector(duty_cycle_vector):
+    x, y, z = duty_cycle_vector
+    x_new = getTimeArr(x)
+    y_new = getTimeArr(y)
+    z_new = getTimeArr(z)
+    temp = np.unique(np.concatenate((x_new, y_new, z_new)))
+    return temp
+    
